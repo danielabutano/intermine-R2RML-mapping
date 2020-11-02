5d76fc6ca1c24b4e9d75e5fa2d280f4597596d23
Daniela Butano <daniela@intermine.org>
2020-11-02 15:54:57 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# R2RML-mapping
+It allows to .....
+Clone the project from github and execute
+```
+cd intermine-R2RML-mapping
+./gradlew install
+./gradlew run'
+```
+
+
---
+++ src/main/java/org/intermine/r2rmlmapping/R2RMLMapping.java
@@ -0,0 +1,8 @@
+package org.intermine.r2rmlmapping;
+
+public class R2RMLMapping {
+
+    public static void main(String[] args) {       
+        System.out.println("R2RMLMapping executed");
+    }
+}
